8db866323c49d45df1c4a6c61c269c98e551aa65
Vedant5125 <vedantapraj5125@gmail.com>
2026-01-09 21:18:00 +0530

    backend server

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+dotenv.config({
+    path: './.env'
+});
+// require('dotenv').config();
+
+const app = express();
+console.log("CORS_ORIGIN is:", process.env.CORS_ORIGIN);
+app.use(cors({
+    origin:(process.env.CORS_ORIGIN),
+    credentials: true
+}))
+
+
+export { app };

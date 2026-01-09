38b071b79b68ce9f2b59a6e22a71e076586cfc57
Vedant5125 <vedantapraj5125@gmail.com>
2026-01-09 23:37:01 +0530

    introduced role for user model

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv"
 dotenv.config({
     path: './.env'
@@ -7,11 +8,15 @@ dotenv.config({
 // require('dotenv').config();
 
 const app = express();
+
 console.log("CORS_ORIGIN is:", process.env.CORS_ORIGIN);
 app.use(cors({
     origin:(process.env.CORS_ORIGIN),
     credentials: true
 }))
+
+app.use(express.json({ limit: "16kb" }));
+app.use(cookieParser());
 
 
 export { app };
